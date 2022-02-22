1a5472309d4d12496fd0c08d0fc006232eabf9cc
Jovana Spasić <87784195+J-Spasic@users.noreply.github.com>
2022-02-22 21:10:21 +0100

    Add methods that set the initial state of the form

--- VirtualTerminal/VirtualTerminal/MainForm.Designer.cs
+++ VirtualTerminal/VirtualTerminal/MainForm.Designer.cs
@@ -29,22 +29,22 @@
         private void InitializeComponent()
         {
             this.panelSettings = new System.Windows.Forms.Panel();
-            this.labelPort = new System.Windows.Forms.Label();
-            this.comboBoxPort = new System.Windows.Forms.ComboBox();
-            this.labelBaudRate = new System.Windows.Forms.Label();
-            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
-            this.labelDataBits = new System.Windows.Forms.Label();
-            this.comboBoxDataBits = new System.Windows.Forms.ComboBox();
-            this.labelParity = new System.Windows.Forms.Label();
-            this.comboBoxParity = new System.Windows.Forms.ComboBox();
-            this.labelStopBits = new System.Windows.Forms.Label();
-            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
-            this.groupBoxBuffer = new System.Windows.Forms.GroupBox();
-            this.radioButtonTextMode = new System.Windows.Forms.RadioButton();
-            this.radioButtonHexMode = new System.Windows.Forms.RadioButton();
-            this.buttonStartTxProcess = new System.Windows.Forms.Button();
-            this.buttonStartRxProcess = new System.Windows.Forms.Button();
             this.richTextBoxTerminal = new System.Windows.Forms.RichTextBox();
+            this.buttonStartRxProcess = new System.Windows.Forms.Button();
+            this.buttonStartTxProcess = new System.Windows.Forms.Button();
+            this.groupBoxBuffer = new System.Windows.Forms.GroupBox();
+            this.radioButtonHexMode = new System.Windows.Forms.RadioButton();
+            this.radioButtonTextMode = new System.Windows.Forms.RadioButton();
+            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
+            this.labelStopBits = new System.Windows.Forms.Label();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.labelParity = new System.Windows.Forms.Label();
+            this.comboBoxDataBits = new System.Windows.Forms.ComboBox();
+            this.labelDataBits = new System.Windows.Forms.Label();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.labelBaudRate = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.labelPort = new System.Windows.Forms.Label();
             this.panelSettings.SuspendLayout();
             this.groupBoxBuffer.SuspendLayout();
             this.SuspendLayout();
@@ -71,32 +71,136 @@
             this.panelSettings.Size = new System.Drawing.Size(484, 211);
             this.panelSettings.TabIndex = 0;
             // 
-            // labelPort
+            // richTextBoxTerminal
             // 
-            this.labelPort.AutoSize = true;
-            this.labelPort.Location = new System.Drawing.Point(12, 15);
-            this.labelPort.Name = "labelPort";
-            this.labelPort.Size = new System.Drawing.Size(32, 15);
-            this.labelPort.TabIndex = 0;
-            this.labelPort.Text = "Port:";
+            this.richTextBoxTerminal.BackColor = System.Drawing.Color.Black;
+            this.richTextBoxTerminal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxTerminal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxTerminal.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBoxTerminal.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBoxTerminal.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxTerminal.Name = "richTextBoxTerminal";
+            this.richTextBoxTerminal.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBoxTerminal.Size = new System.Drawing.Size(484, 211);
+            this.richTextBoxTerminal.TabIndex = 13;
+            this.richTextBoxTerminal.Text = "> Note: Enter vt -quit to go back to main screen.";
             // 
-            // comboBoxPort
+            // buttonStartRxProcess
             // 
-            this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxPort.FormattingEnabled = true;
-            this.comboBoxPort.Location = new System.Drawing.Point(125, 12);
-            this.comboBoxPort.Name = "comboBoxPort";
-            this.comboBoxPort.Size = new System.Drawing.Size(121, 23);
-            this.comboBoxPort.TabIndex = 1;
+            this.buttonStartRxProcess.Location = new System.Drawing.Point(255, 165);
+            this.buttonStartRxProcess.Name = "buttonStartRxProcess";
+            this.buttonStartRxProcess.Size = new System.Drawing.Size(120, 35);
+            this.buttonStartRxProcess.TabIndex = 12;
+            this.buttonStartRxProcess.Text = "Start Rx Process";
+            this.buttonStartRxProcess.UseVisualStyleBackColor = true;
             // 
-            // labelBaudRate
+            // buttonStartTxProcess
             // 
-            this.labelBaudRate.AutoSize = true;
-            this.labelBaudRate.Location = new System.Drawing.Point(12, 44);
-            this.labelBaudRate.Name = "labelBaudRate";
-            this.labelBaudRate.Size = new System.Drawing.Size(63, 15);
-            this.labelBaudRate.TabIndex = 2;
-            this.labelBaudRate.Text = "Baud Rate:";
+            this.buttonStartTxProcess.Location = new System.Drawing.Point(115, 165);
+            this.buttonStartTxProcess.Name = "buttonStartTxProcess";
+            this.buttonStartTxProcess.Size = new System.Drawing.Size(120, 35);
+            this.buttonStartTxProcess.TabIndex = 11;
+            this.buttonStartTxProcess.Text = "Start Tx Process";
+            this.buttonStartTxProcess.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxBuffer
+            // 
+            this.groupBoxBuffer.Controls.Add(this.radioButtonHexMode);
+            this.groupBoxBuffer.Controls.Add(this.radioButtonTextMode);
+            this.groupBoxBuffer.Location = new System.Drawing.Point(285, 30);
+            this.groupBoxBuffer.Name = "groupBoxBuffer";
+            this.groupBoxBuffer.Size = new System.Drawing.Size(187, 100);
+            this.groupBoxBuffer.TabIndex = 10;
+            this.groupBoxBuffer.TabStop = false;
+            this.groupBoxBuffer.Text = "Buffer";
+            // 
+            // radioButtonHexMode
+            // 
+            this.radioButtonHexMode.AutoSize = true;
+            this.radioButtonHexMode.Location = new System.Drawing.Point(60, 55);
+            this.radioButtonHexMode.Name = "radioButtonHexMode";
+            this.radioButtonHexMode.Size = new System.Drawing.Size(80, 19);
+            this.radioButtonHexMode.TabIndex = 1;
+            this.radioButtonHexMode.Text = "Hex Mode";
+            this.radioButtonHexMode.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTextMode
+            // 
+            this.radioButtonTextMode.AutoSize = true;
+            this.radioButtonTextMode.Checked = true;
+            this.radioButtonTextMode.Location = new System.Drawing.Point(60, 30);
+            this.radioButtonTextMode.Name = "radioButtonTextMode";
+            this.radioButtonTextMode.Size = new System.Drawing.Size(80, 19);
+            this.radioButtonTextMode.TabIndex = 0;
+            this.radioButtonTextMode.TabStop = true;
+            this.radioButtonTextMode.Text = "Text Mode";
+            this.radioButtonTextMode.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxStopBits
+            // 
+            this.comboBoxStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxStopBits.FormattingEnabled = true;
+            this.comboBoxStopBits.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBoxStopBits.Location = new System.Drawing.Point(125, 128);
+            this.comboBoxStopBits.Name = "comboBoxStopBits";
+            this.comboBoxStopBits.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxStopBits.TabIndex = 9;
+            // 
+            // labelStopBits
+            // 
+            this.labelStopBits.AutoSize = true;
+            this.labelStopBits.Location = new System.Drawing.Point(12, 131);
+            this.labelStopBits.Name = "labelStopBits";
+            this.labelStopBits.Size = new System.Drawing.Size(56, 15);
+            this.labelStopBits.TabIndex = 8;
+            this.labelStopBits.Text = "Stop Bits:";
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Items.AddRange(new object[] {
+            "NONE",
+            "EVEN",
+            "ODD",
+            "MARK",
+            "SPACE"});
+            this.comboBoxParity.Location = new System.Drawing.Point(125, 99);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxParity.TabIndex = 7;
+            // 
+            // labelParity
+            // 
+            this.labelParity.AutoSize = true;
+            this.labelParity.Location = new System.Drawing.Point(12, 102);
+            this.labelParity.Name = "labelParity";
+            this.labelParity.Size = new System.Drawing.Size(40, 15);
+            this.labelParity.TabIndex = 6;
+            this.labelParity.Text = "Parity:";
+            // 
+            // comboBoxDataBits
+            // 
+            this.comboBoxDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDataBits.FormattingEnabled = true;
+            this.comboBoxDataBits.Items.AddRange(new object[] {
+            "7",
+            "8"});
+            this.comboBoxDataBits.Location = new System.Drawing.Point(125, 70);
+            this.comboBoxDataBits.Name = "comboBoxDataBits";
+            this.comboBoxDataBits.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxDataBits.TabIndex = 5;
+            // 
+            // labelDataBits
+            // 
+            this.labelDataBits.AutoSize = true;
+            this.labelDataBits.Location = new System.Drawing.Point(12, 73);
+            this.labelDataBits.Name = "labelDataBits";
+            this.labelDataBits.Size = new System.Drawing.Size(56, 15);
+            this.labelDataBits.TabIndex = 4;
+            this.labelDataBits.Text = "Data Bits:";
             // 
             // comboBoxBaudRate
             // 
@@ -117,137 +221,32 @@
             this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 23);
             this.comboBoxBaudRate.TabIndex = 3;
             // 
-            // labelDataBits
+            // labelBaudRate
             // 
-            this.labelDataBits.AutoSize = true;
-            this.labelDataBits.Location = new System.Drawing.Point(12, 73);
-            this.labelDataBits.Name = "labelDataBits";
-            this.labelDataBits.Size = new System.Drawing.Size(56, 15);
-            this.labelDataBits.TabIndex = 4;
-            this.labelDataBits.Text = "Data Bits:";
+            this.labelBaudRate.AutoSize = true;
+            this.labelBaudRate.Location = new System.Drawing.Point(12, 44);
+            this.labelBaudRate.Name = "labelBaudRate";
+            this.labelBaudRate.Size = new System.Drawing.Size(63, 15);
+            this.labelBaudRate.TabIndex = 2;
+            this.labelBaudRate.Text = "Baud Rate:";
             // 
-            // comboBoxDataBits
+            // comboBoxPort
             // 
-            this.comboBoxDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxDataBits.FormattingEnabled = true;
-            this.comboBoxDataBits.Items.AddRange(new object[] {
-            "7",
-            "8"});
-            this.comboBoxDataBits.Location = new System.Drawing.Point(125, 70);
-            this.comboBoxDataBits.Name = "comboBoxDataBits";
-            this.comboBoxDataBits.Size = new System.Drawing.Size(121, 23);
-            this.comboBoxDataBits.TabIndex = 5;
+            this.comboBoxPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(125, 12);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxPort.TabIndex = 1;
             // 
-            // labelParity
+            // labelPort
             // 
-            this.labelParity.AutoSize = true;
-            this.labelParity.Location = new System.Drawing.Point(12, 102);
-            this.labelParity.Name = "labelParity";
-            this.labelParity.Size = new System.Drawing.Size(40, 15);
-            this.labelParity.TabIndex = 6;
-            this.labelParity.Text = "Parity:";
-            // 
-            // comboBoxParity
-            // 
-            this.comboBoxParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxParity.FormattingEnabled = true;
-            this.comboBoxParity.Items.AddRange(new object[] {
-            "NONE",
-            "EVEN",
-            "ODD",
-            "MARK",
-            "SPACE"});
-            this.comboBoxParity.Location = new System.Drawing.Point(125, 99);
-            this.comboBoxParity.Name = "comboBoxParity";
-            this.comboBoxParity.Size = new System.Drawing.Size(121, 23);
-            this.comboBoxParity.TabIndex = 7;
-            // 
-            // labelStopBits
-            // 
-            this.labelStopBits.AutoSize = true;
-            this.labelStopBits.Location = new System.Drawing.Point(12, 131);
-            this.labelStopBits.Name = "labelStopBits";
-            this.labelStopBits.Size = new System.Drawing.Size(56, 15);
-            this.labelStopBits.TabIndex = 8;
-            this.labelStopBits.Text = "Stop Bits:";
-            // 
-            // comboBoxStopBits
-            // 
-            this.comboBoxStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxStopBits.FormattingEnabled = true;
-            this.comboBoxStopBits.Items.AddRange(new object[] {
-            "1",
-            "2"});
-            this.comboBoxStopBits.Location = new System.Drawing.Point(125, 128);
-            this.comboBoxStopBits.Name = "comboBoxStopBits";
-            this.comboBoxStopBits.Size = new System.Drawing.Size(121, 23);
-            this.comboBoxStopBits.TabIndex = 9;
-            // 
-            // groupBoxBuffer
-            // 
-            this.groupBoxBuffer.Controls.Add(this.radioButtonHexMode);
-            this.groupBoxBuffer.Controls.Add(this.radioButtonTextMode);
-            this.groupBoxBuffer.Location = new System.Drawing.Point(285, 30);
-            this.groupBoxBuffer.Name = "groupBoxBuffer";
-            this.groupBoxBuffer.Size = new System.Drawing.Size(187, 100);
-            this.groupBoxBuffer.TabIndex = 10;
-            this.groupBoxBuffer.TabStop = false;
-            this.groupBoxBuffer.Text = "Buffer";
-            // 
-            // radioButtonTextMode
-            // 
-            this.radioButtonTextMode.AutoSize = true;
-            this.radioButtonTextMode.Checked = true;
-            this.radioButtonTextMode.Location = new System.Drawing.Point(60, 30);
-            this.radioButtonTextMode.Name = "radioButtonTextMode";
-            this.radioButtonTextMode.Size = new System.Drawing.Size(80, 19);
-            this.radioButtonTextMode.TabIndex = 0;
-            this.radioButtonTextMode.TabStop = true;
-            this.radioButtonTextMode.Text = "Text Mode";
-            this.radioButtonTextMode.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonHexMode
-            // 
-            this.radioButtonHexMode.AutoSize = true;
-            this.radioButtonHexMode.Location = new System.Drawing.Point(60, 55);
-            this.radioButtonHexMode.Name = "radioButtonHexMode";
-            this.radioButtonHexMode.Size = new System.Drawing.Size(80, 19);
-            this.radioButtonHexMode.TabIndex = 1;
-            this.radioButtonHexMode.Text = "Hex Mode";
-            this.radioButtonHexMode.UseVisualStyleBackColor = true;
-            // 
-            // buttonStartTxProcess
-            // 
-            this.buttonStartTxProcess.Location = new System.Drawing.Point(115, 165);
-            this.buttonStartTxProcess.Name = "buttonStartTxProcess";
-            this.buttonStartTxProcess.Size = new System.Drawing.Size(120, 35);
-            this.buttonStartTxProcess.TabIndex = 11;
-            this.buttonStartTxProcess.Text = "Start Tx Process";
-            this.buttonStartTxProcess.UseVisualStyleBackColor = true;
-            // 
-            // buttonStartRxProcess
-            // 
-            this.buttonStartRxProcess.Location = new System.Drawing.Point(255, 165);
-            this.buttonStartRxProcess.Name = "buttonStartRxProcess";
-            this.buttonStartRxProcess.Size = new System.Drawing.Size(120, 35);
-            this.buttonStartRxProcess.TabIndex = 12;
-            this.buttonStartRxProcess.Text = "Start Rx Process";
-            this.buttonStartRxProcess.UseVisualStyleBackColor = true;
-            // 
-            // richTextBoxTerminal
-            // 
-            this.richTextBoxTerminal.BackColor = System.Drawing.Color.Black;
-            this.richTextBoxTerminal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.richTextBoxTerminal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBoxTerminal.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.richTextBoxTerminal.ForeColor = System.Drawing.Color.Lime;
-            this.richTextBoxTerminal.Location = new System.Drawing.Point(0, 0);
-            this.richTextBoxTerminal.Name = "richTextBoxTerminal";
-            this.richTextBoxTerminal.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxTerminal.Size = new System.Drawing.Size(484, 211);
-            this.richTextBoxTerminal.TabIndex = 13;
-            this.richTextBoxTerminal.Text = "> Note: Enter vt -quit to go back to main screen.";
-            this.richTextBoxTerminal.Visible = false;
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(12, 15);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(32, 15);
+            this.labelPort.TabIndex = 0;
+            this.labelPort.Text = "Port:";
             // 
             // MainForm
             // 
@@ -260,6 +259,7 @@
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Virtual Terminal";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.panelSettings.ResumeLayout(false);
             this.panelSettings.PerformLayout();
             this.groupBoxBuffer.ResumeLayout(false);
--- VirtualTerminal/VirtualTerminal/MainForm.cs
+++ VirtualTerminal/VirtualTerminal/MainForm.cs
@@ -1,12 +1,47 @@
+using System;
 using System.Windows.Forms;
 
 namespace VirtualTerminal
 {
     public partial class MainForm : Form
     {
+        #region Constructor(s)
         public MainForm()
         {
             InitializeComponent();
         }
+        #endregion Constructor(s)
+
+        #region Event(s)
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetInitialStateOfControls();
+        }
+        #endregion Event(s)
+
+        #region Method(s)
+        private void SetInitialStateOfControls()
+        {
+            SetComboBoxesDefaultValues();
+
+            SetButtonsEnabledProperty(false);
+
+            richTextBoxTerminal.Visible = false;
+        }
+
+        private void SetComboBoxesDefaultValues()
+        {
+            comboBoxBaudRate.SelectedIndex = 3; // 1200
+            comboBoxDataBits.SelectedIndex = 1; // 8
+            comboBoxParity.SelectedIndex = 1;   // EVEN
+            comboBoxStopBits.SelectedIndex = 1; // 2
+        }
+
+        private void SetButtonsEnabledProperty(bool enableButton)
+        {
+            buttonStartTxProcess.Enabled = enableButton;
+            buttonStartRxProcess.Enabled = enableButton;
+        }
+        #endregion Method(s)
     }
 }
