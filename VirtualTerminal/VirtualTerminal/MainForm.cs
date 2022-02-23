ac52cea16f75550b4ad341a0cc2c6b14a6d52570
Jovana Spasić <87784195+J-Spasic@users.noreply.github.com>
2022-02-23 13:00:21 +0100

    Add methods for basic port configuration

--- VirtualTerminal/VirtualTerminal/MainForm.cs
+++ VirtualTerminal/VirtualTerminal/MainForm.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Windows.Forms;
 
+using VirtualTerminal.Services;
+
 namespace VirtualTerminal
 {
     public partial class MainForm : Form
@@ -15,16 +17,44 @@ namespace VirtualTerminal
         #region Event(s)
         private void MainForm_Load(object sender, EventArgs e)
         {
+            SetPortComboBox();
             SetInitialStateOfControls();
         }
         #endregion Event(s)
 
         #region Method(s)
+        private void SetPortComboBox()
+        {
+            string[] serialPortNames = VirtualTerminalService.GetAvailableSerialPortNames();
+
+            if (serialPortNames.Length == 0)
+            {
+                MessageBox.Show("You do not have available serial ports.", "No serial ports available",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                foreach (string portName in serialPortNames)
+                {
+                    comboBoxPort.Items.Add(portName);
+                }
+
+                comboBoxPort.SelectedIndex = 0;
+            }
+        }
+
         private void SetInitialStateOfControls()
         {
             SetComboBoxesDefaultValues();
 
-            SetButtonsEnabledProperty(false);
+            if (comboBoxPort.Items.Count > 0)
+            {
+                SetButtonsEnabledProperty(true);
+            }
+            else
+            {
+                SetButtonsEnabledProperty(false);
+            }
 
             richTextBoxTerminal.Visible = false;
         }
---
+++ VirtualTerminal/VirtualTerminal/Services/VirtualTerminalService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Ports;
+
+namespace VirtualTerminal.Services
+{
+    internal static class VirtualTerminalService
+    {
+        #region Field(s)
+        private static readonly SerialPort serialPort;
+        #endregion Field(s)
+
+        #region Constructor(s)
+        static VirtualTerminalService()
+        {
+            VirtualTerminalService.serialPort = new SerialPort();
+        }
+        #endregion Constructor(s)
+
+        #region Method(s)
+        public static string[] GetAvailableSerialPortNames()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public static void ConfigureSerialPort(string portName, int baudRate, int parityIndex,
+            int dataBits, int stopBit)
+        {
+            VirtualTerminalService.serialPort.PortName = portName;
+            VirtualTerminalService.serialPort.BaudRate = baudRate;
+            VirtualTerminalService.serialPort.Parity = (Parity)parityIndex;
+            VirtualTerminalService.serialPort.DataBits = dataBits;
+            VirtualTerminalService.serialPort.StopBits = (StopBits)stopBit;
+        }
+        #endregion Method(s)
+    }
+}
